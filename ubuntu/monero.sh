e55a71b1a589d39b696983bfc66f4e07d706d2d1
coleTitze <74699914+coleTitze@users.noreply.github.com>
2021-05-10 17:23:11 -0500

    Update monero.sh

--- ubuntu/monero.sh
+++ ubuntu/monero.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ~/xmrig/build/
+cd ~/mining/xmrig/build/
 echo 1024 | sudo tee /proc/sys/vm/nr_hugepages
 sudo ./xmrig -o us-west.minexmr.com:4444 -u 44pxH6jZCRfjfPFLFy6bmi5hqZYAt7m5bcUQKfcQjKgsam4WS13cL2sLgX9KPcMmGbHM1vBzjdgEsTSZBL6sC7ruM1AEGvu.pc -a rx/0 -k

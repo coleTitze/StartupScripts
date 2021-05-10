ab04d1169ba6541771c159c6384d01c69f2b15ea
coleTitze <74699914+coleTitze@users.noreply.github.com>
2021-05-10 16:45:58 -0500

    Update ubuSetup.sh

--- ubuntu/ubuSetup.sh
+++ ubuntu/ubuSetup.sh
@@ -9,7 +9,7 @@ ssh-keygen -t ed25519 -C "cole.titze@mines.sdsmt.edu"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 sudo apt-get install lm-sensors
-sudo apt-get install psensors
+sudo apt-get install psensor
 ./ubuRandomX.sh
 echo "alias monero='~/StartupScripts/ubuntu/monero.sh'" >> ~/.bash_aliases
 echo "alias qrl='~/StartupScripts/ubuntu/qrl.sh'" >> ~/.bash_aliases

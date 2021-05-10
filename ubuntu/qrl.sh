edef7dd9d9ee5e29a6f7076216b817d9dc9cbadf
Cole <cole.titze@mines.sdsmt.edu>
2021-05-09 22:35:44 -0600

    Added scripts

---
+++ ubuntu/qrl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ~/xmrig/build/
+echo 1024 | sudo tee /proc/sys/vm/nr_hugepages
+sudo ./xmrig -o qrl.herominers.com:10371 -u Q010500c82e4b0a4568ba8baebe93ed7e3d45d13ec5457e03a5a7583d43af305289e923629df7ad -p PC -a rx/0 -k
---
+++ ubuntu/ubuMonero.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev screen
+mkdir mining
+cd mining
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig && mkdir build && cd build
+cmake ..
+make
+cd

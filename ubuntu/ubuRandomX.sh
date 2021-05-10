51a564f3c530d5399410ad50de5a2bd9f7172de9
coleTitze <74699914+coleTitze@users.noreply.github.com>
2021-05-10 17:15:14 -0500

    Update ubuRandomX.sh

--- ubuntu/ubuRandomX.sh
+++ ubuntu/ubuRandomX.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev screen
 mkdir ~/mining
-cd mining
+cd ~/mining
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
 cmake ..

e8d96b3e3581b232d33ecfae181968da46a3e7d3
coleTitze <74699914+coleTitze@users.noreply.github.com>
2021-05-09 23:28:24 -0600

    Update ubuSetup.sh

--- ubuntu/ubuSetup.sh
+++ ubuntu/ubuSetup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sudo snap install spotify
+sudo snap install --classic code
+sudo snap install pycharm-community --classic
+sudo snap install intellij-idea-community --classic
+sudo snap install phpstorm --classic
+sudo snap install webstorm --classic
 ssh-keygen -t ed25519 -C "cole.titze@mines.sdsmt.edu"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
